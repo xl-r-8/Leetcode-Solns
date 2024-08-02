e368a30fea7d5584f638ce5d461a86c820b6df52
abhi9-arya <abhi9arya8@gmail.com>
2024-08-03 01:08:48 +0530

    Time: 46 ms (50.42%) | Memory: 51 MB (43.21%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        // vector<int> p;
         int farthest=-1;
         int i=0;
         while (i<nums.size()){
@@ -9,16 +10,30 @@ public:
             else if(jump==0) return false;
             for(int j=i+1; j<=i+jump;j++){
                 if(j==nums.size()-1) return true;
+                // if(p.empty()){
+                //     p.push_back(j);
+                // }
                 if(farthest==-1){
                     farthest=j;
                 }
                 else{
-                    
+                    // int index=p.back();
+                    // if(nums[j]+j>=nums[index]+index){
+                    //     p.push_back(j);
+                    // }
+                    // else{
+                    //     p.insert(p.begin(),j); 
+                    // }
                     if(nums[j]+j>=nums[farthest]+farthest){
                         farthest=j;
-                    }       
+                    }
+                    // else{
+                    //     p.insert(p.begin(),j); 
+                    // }
                 }
             }
+            // i=p.back();
+            // p.erase(p.begin()+p.size()-1);
             i=farthest;
        }
        return false;
