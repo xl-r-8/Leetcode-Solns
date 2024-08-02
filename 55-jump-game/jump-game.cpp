9b332311e10dd2978c37c97931fe74ad630c83f2
abhi9-arya <abhi9arya8@gmail.com>
2024-08-03 01:06:42 +0530

    Time: 46 ms (50.42%) | Memory: 51.2 MB (28.38%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> p;
+        // vector<int> p;
+        int farthest=-1;
         int i=0;
         while (i<nums.size()){
             int jump=nums[i];
@@ -9,21 +10,31 @@ public:
             else if(jump==0) return false;
             for(int j=i+1; j<=i+jump;j++){
                 if(j==nums.size()-1) return true;
-                if(p.empty()){
-                    p.push_back(j);
+                // if(p.empty()){
+                //     p.push_back(j);
+                // }
+                if(farthest==-1){
+                    farthest=j;
                 }
                 else{
-                    int index=p.back();
-                    if(nums[j]+j>=nums[index]+index){
-                        p.push_back(j);
+                    // int index=p.back();
+                    // if(nums[j]+j>=nums[index]+index){
+                    //     p.push_back(j);
+                    // }
+                    // else{
+                    //     p.insert(p.begin(),j); 
+                    // }
+                    if(nums[j]+j>=nums[farthest]+farthest){
+                        farthest=j;
                     }
-                    else{
-                        p.insert(p.begin(),j); 
-                    }
+                    // else{
+                    //     p.insert(p.begin(),j); 
+                    // }
                 }
             }
-            i=p.back();
-            p.erase(p.begin()+p.size()-1);
+            // i=p.back();
+            // p.erase(p.begin()+p.size()-1);
+            i=farthest;
        }
        return false;
     }
