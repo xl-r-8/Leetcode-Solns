ceef9a33e0e25cbbf06ce0634b5b09c88bd71634
abhi9-arya <abhi9arya8@gmail.com>
2024-08-03 01:07:56 +0530

    Time: 48 ms (41.45%) | Memory: 51.1 MB (43.21%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -10,30 +10,19 @@ public:
             else if(jump==0) return false;
             for(int j=i+1; j<=i+jump;j++){
                 if(j==nums.size()-1) return true;
-                // if(p.empty()){
-                //     p.push_back(j);
-                // }
+                
                 if(farthest==-1){
                     farthest=j;
                 }
                 else{
-                    // int index=p.back();
-                    // if(nums[j]+j>=nums[index]+index){
-                    //     p.push_back(j);
-                    // }
-                    // else{
-                    //     p.insert(p.begin(),j); 
-                    // }
+                    
                     if(nums[j]+j>=nums[farthest]+farthest){
                         farthest=j;
                     }
-                    // else{
-                    //     p.insert(p.begin(),j); 
-                    // }
+                    
                 }
             }
-            // i=p.back();
-            // p.erase(p.begin()+p.size()-1);
+            
             i=farthest;
        }
        return false;
