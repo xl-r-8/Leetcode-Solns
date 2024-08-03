3473c34aa726e5565ada0e21ea2f2ed9583e313b
abhi9-arya <abhi9arya8@gmail.com>
2024-08-04 00:47:53 +0530

    Time: 7 ms (84.20%) | Memory: 20.1 MB (13.35%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        //M1: TC= O(2nlogn+n)=O(nlogn)
+        // if(target.size()!=arr.size()) return false;
+        // sort(target.begin(),target.end());
+        // sort(arr.begin(),arr.end());
+        // for(int i=0; i<target.size();i++){
+        //     if(target[i]!=arr[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        //M2: TC: O(n)
         if(target.size()!=arr.size()) return false;
-        sort(target.begin(),target.end());
-        sort(arr.begin(),arr.end());
-        for(int i=0; i<target.size();i++){
-            if(target[i]!=arr[i]){
-                return false;
-            }
+        unordered_map<int,int> mp;
+        for(int i=0;i<target.size();i++){
+            ++mp[target[i]];
+        }
+        for(int i=0;i<target.size();i++){
+            --mp[arr[i]];
+        }
+        for(pair<int,int> p: mp){
+            if(p.second>0) return false;
         }
         return true;
+
+
     }
 };
