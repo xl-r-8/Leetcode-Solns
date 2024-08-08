86a11968f2c1115c5df5660a0a51541e36960008
abhi9-arya <abhi9arya8@gmail.com>
2024-08-09 02:09:53 +0530

    Time: 12 ms (69.93%) | Memory: 11.9 MB (45.75%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.strip().split()
+        
+        if not words:
+            return 0
+        
+        return len(words[-1])
