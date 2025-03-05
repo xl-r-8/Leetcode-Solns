c9f261f9cb74e8010ea50741a50f75d8e66ed28e
abhi9-arya <abhi9arya8@gmail.com>
2025-03-06 02:12:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -22,17 +22,31 @@ public:
     } */
 
     //best approach without memory optimization
+    // int climbStairs(int n) {
+    //     if(n<=2) return n;
+    //     int dp[n+1];
+    //     dp[0]=0;
+    //     dp[1]=1;
+    //     dp[2]=2;
+        
+    //     for(int i=3; i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+
+    // } 
+
     int climbStairs(int n) {
         if(n<=2) return n;
-        int dp[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+        int a=1;
+        int b=2;
         
         for(int i=3; i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int temp=a+b;
+            a=b;
+            b=temp;
         }
-        return dp[n];
+        return b;
 
     } 
 };
