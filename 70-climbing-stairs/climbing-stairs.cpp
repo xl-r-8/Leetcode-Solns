9af0b32cf32144b14ffedbb25c6511d5b9c994e9
abhi9-arya <abhi9arya8@gmail.com>
2025-03-06 02:11:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,38 @@
-int solve3(int n){
-    if(n==0) return 1;
-    if(n<=3 and n>=1) return n;
-    int a=2;
-    int b=3;
-    for(int i=4;i<=n;i++){
-        int temp=a+b;
-        a=b;
-        b=temp;
-    }
-    return b;
-}
 class Solution {
 public:
-    int climbStairs(int n) {
-        return solve3(n);
+    //brute force
+    /* int climbStairs(int n) {
+        if(n<=2) return n;
+        int ways=climbStairs(n-1)+climbStairs(n-2);
+        return ways;
+    } */
+    //better approach
+    /* int recfunc(int n,int dp[]){
+        if(n<=2) return n;
+        if(dp[n]!=0) return dp[n];
+        int ways=climbStairs(n-1)+climbStairs(n-2);
+        return dp[n]=ways;
     }
+    int climbStairs(int n) {
+        int dp[n+1];
+        for(int i=0; i<=n; i++){
+            dp[i]=0;
+        }
+        return recfunc (n,dp);
+    } */
+
+    //best approach without memory optimization
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3; i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
+    } 
 };
