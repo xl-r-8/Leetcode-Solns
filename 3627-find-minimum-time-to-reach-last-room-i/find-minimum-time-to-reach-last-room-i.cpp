c17f3957b665d49010f282168ef1c311451f6448
Abhinav Arya <abhi9arya8@gmail.com>
2025-05-08 15:39:30 +0530

    Time: 144 ms (5.12%) | Memory: 58.3 MB (5.23%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -13,48 +13,65 @@ public:
         vector<vector<int>> cost(n, vector<int>(m, -1));
         vector<vector<bool>> visited(n, vector<bool>(m, false));
         cost[0][0]=0;
-        visited[0][0]=true;
         while(!pq.empty()){
             vector<int> ele=pq.top();
             pq.pop();
             int i=ele[0], j=ele[1], eleC=ele[2];
-            if(eleC<cost[i][j] or cost[i][j]==-1) cost[i][j]=eleC;
+            // if(eleC<cost[i][j] or cost[i][j]==-1) cost[i][j]=eleC;
+            if(visited[i][j]) continue;
             if(i==n-1 and j==m-1) break;
+            visited[i][j]=true;
             if(i-1>=0 and visited[i-1][j]==false){//up
                 int cst=0;
+                int addition=1;
                 if(eleC>=moveTime[i-1][j]){
-                    cst=eleC+1;
+                    cst=eleC+addition;
                 }
-                else cst= moveTime[i-1][j]+1;
-                pq.push({i-1,j,cst});
-                visited[i-1][j]=true;
+                else cst= moveTime[i-1][j]+addition;
+                if(cst<cost[i-1][j] or cost[i-1][j]==-1){
+                    cost[i-1][j]=cst;
+                    pq.push({i-1,j,cst});
+                }
+                // visited[i-1][j]=true;
             }
-            if(i+1<n and visited[i+1][j]==false){//down
+            if(i+1<n and visited[i+1][j]==false ){//down
                 int cst=0;
+                int addition=1; 
                 if(eleC>=moveTime[i+1][j]){
-                    cst=eleC+1;
+                    cst=eleC+addition;
                 }
-                else cst= moveTime[i+1][j]+1;
-                pq.push({i+1,j,cst});
-                visited[i+1][j]=true;
+                else cst= moveTime[i+1][j]+addition;
+                if(cst<cost[i+1][j] or cost[i+1][j]==-1){
+                    cost[i+1][j]=cst;
+                    pq.push({i+1,j,cst});
+                }
+                // visited[i+1][j]=true;
             }
             if(j-1>=0 and visited[i][j-1]==false){//left
                 int cst=0;
+                int addition=1;
                 if(eleC>=moveTime[i][j-1]){
-                    cst=eleC+1;
+                    cst=eleC+addition;
                 }
-                else cst= moveTime[i][j-1]+1;
-                pq.push({i,j-1,cst});
-                visited[i][j-1]=true;
+                else cst= moveTime[i][j-1]+addition;
+                if(cst<cost[i][j-1] or cost[i][j-1]==-1){
+                    cost[i][j-1]=cst;
+                    pq.push({i,j-1,cst});
+                }
+                // visited[i][j-1]=true;
             }
             if(j+1<m and visited[i][j+1]==false){//right
                 int cst=0;
+                int addition=1;
                 if(eleC>=moveTime[i][j+1]){
-                    cst=eleC+1;
+                    cst=eleC+addition;
                 }
-                else cst= moveTime[i][j+1]+1;
-                pq.push({i,j+1,cst});
-                visited[i][j+1]=true;
+                else cst= moveTime[i][j+1]+addition;
+                if(cst<cost[i][j+1] or cost[i][j+1]==-1){
+                    cost[i][j+1]=cst;
+                    pq.push({i,j+1,cst});
+                }
+                // visited[i][j+1]=true;
             }
 
         }
