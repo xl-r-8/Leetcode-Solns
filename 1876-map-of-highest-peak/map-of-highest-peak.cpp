8c3fb4702d24f67b2b4f95051fc156a8056bfc2c
Abhinav Arya <abhi9arya8@gmail.com>
2025-05-13 23:01:14 +0530

    Time: 67 ms (86.05%) | Memory: 112.8 MB (84.04%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        vector<vector<int>> lvl(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+
+        // Step 1: Push all 0s into queue and set lvl = 0
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isWater[i][j] == 1) {
+                    lvl[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        // Directions for moving up, down, left, right
+        vector<pair<int, int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        while (!q.empty()){
+            auto [i, j] = q.front();
+            q.pop();
+
+            for (auto [di, dj] : directions) {
+                int ni = i + di, nj = j + dj;
+                // If in bounds and not visited
+                if (ni >= 0 && nj >= 0 && ni < m && nj < n && lvl[ni][nj] == -1) {
+                    lvl[ni][nj] = lvl[i][j] + 1;
+                    q.push({ni, nj});
+                }
+            }
+        }
+        
+        return lvl;
+    }
+};
