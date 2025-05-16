6a6d8c24536d94011271e99e1c815ae5e1352481
Abhinav Arya <abhi9arya8@gmail.com>
2025-05-17 01:14:56 +0530

    Time: 19 ms (96.08%) | Memory: 16.8 MB (58.07%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,38 @@
+void dfsrec(vector<vector<char>>& grid, vector<vector<bool>>& check, int i, int j) {
+    int m = grid.size();
+    int n = grid[0].size();
+    
+    if (i < 0 || j < 0 || i >= m || j >= n || check[i][j] || grid[i][j] == '0') return;
+
+    check[i][j] = true; 
+
+    dfsrec(grid, check, i, j - 1); // left
+    dfsrec(grid, check, i, j + 1); // right
+    dfsrec(grid, check, i - 1, j); // up
+    dfsrec(grid, check, i + 1, j); // down
+
+}
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> check(m, vector<bool>(n, false));
+        int totalc = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!check[i][j]) {
+                    if (grid[i][j] == '0') {
+                        check[i][j] = true;
+                    } else {
+                        dfsrec(grid, check, i, j);
+                        totalc ++;
+                    }
+                }
+            }
+        }
+        return totalc;
+    }
+};
