6068deda9f1cdbe3027b51716cb7acaaa069df21
Abhinav Arya <abhi9arya8@gmail.com>
2025-05-19 03:03:16 +0530

    Time: 4 ms (36.51%) | Memory: 12.1 MB (62.45%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -18,35 +18,55 @@ public:
     }
 };
 */
-// void dfs(Node* node, vector<vector<int>>& graph, vector<bool>& visited){
+void dfs(pair<Node*,Node*> p,  unordered_map<Node*,Node*>& mp){
+    Node* ogNode=p.first;
+    Node* cloneNode=p.second;
+    mp[ogNode]=cloneNode;
+    for(Node* ogNeighbor: ogNode->neighbors){
+        if(mp[ogNeighbor]==nullptr){
+            Node* cloneNeighbor=new Node(ogNeighbor->val);
+            cloneNode->neighbors.push_back(cloneNeighbor);
+            dfs({ogNeighbor,cloneNeighbor},mp);
+        }
+        else cloneNode->neighbors.push_back(mp[ogNeighbor]);
+    }
+}
 
-// }
+Node* bfs(Node* node){
+    Node* head = new Node(node->val);
+    // Node* temp=head;
+    queue<pair<Node*,Node*>> q;
+    q.push({node,head});
+    unordered_map<Node*,Node*> mp;
+    //why did i change from <Node*, bool> to <Node*, Node*>?
+    mp[node]=head;
+    while(!q.empty()){
+        auto p=q.front();q.pop();//current node of og graph
+        Node* ogNode=p.first;
+        Node* cloneNode=p.second;
+        for(Node* ogNeighbor: ogNode->neighbors){
+            if(mp[ogNeighbor]==nullptr){
+                Node* cloneNeighbor=new Node(ogNeighbor->val);
+                cloneNode->neighbors.push_back(cloneNeighbor);
+                mp[ogNeighbor]=cloneNeighbor;
+                q.push({ogNeighbor,cloneNeighbor});
+            }
+            else cloneNode->neighbors.push_back(mp[ogNeighbor]);
+            // why did i add an else?
+        }
+    }
+    return head;
+}
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
         if(node==nullptr){
             return nullptr;
         }
+        // return bfs(node);
         Node* head = new Node(node->val);
-        // Node* temp=head;
-        queue<pair<Node*,Node*>> q;
-        q.push({node,head});
-        unordered_map<Node*,Node*> mp;
-        mp[node]=head;
-        while(!q.empty()){
-            auto p=q.front();q.pop();//current node of og graph
-            Node* ogNode=p.first;
-            Node* cloneNode=p.second;
-            for(Node* ogNeighbor: ogNode->neighbors){
-                if(mp[ogNeighbor]==nullptr){
-                    Node* cloneNeighbor=new Node(ogNeighbor->val);
-                    cloneNode->neighbors.push_back(cloneNeighbor);
-                    mp[ogNeighbor]=cloneNeighbor;
-                    q.push({ogNeighbor,cloneNeighbor});
-                }
-                else cloneNode->neighbors.push_back(mp[ogNeighbor]);
-            }
-        }
+        unordered_map<Node*,Node*> mp;//unordered_map<Node*&,Node*&> mp; is this allowed? actually this is not needed coz you are already sending Node* A POINTER and not a node. it's like sending an array, you dont need & for it.
+        dfs({node,head},mp);
         return head;
     }
 };
