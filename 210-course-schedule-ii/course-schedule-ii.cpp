5b5260afa9f51cab6ea737efe7c3a541087a73a2
Abhinav Arya <abhi9arya8@gmail.com>
2025-05-25 15:21:15 +0530

    Time: 12 ms (9.16%) | Memory: 20.3 MB (6.05%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,3 +1,18 @@
+bool dfs(int node, vector<vector<int>>& adjl, vector<bool>& vis, vector<bool>& pathvis, vector<int> & storage){
+    vis[node] = true;
+    pathvis[node]=true;
+    for(int neighbor : adjl[node]){
+        if(!vis[neighbor]){
+            if (dfs(neighbor, adjl, vis, pathvis,storage)) return true;
+        }
+        if(pathvis[neighbor]) return true;
+    }
+    storage.push_back(node);
+    pathvis[node]=false;
+    return false;
+    
+}
+
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
@@ -12,43 +27,42 @@ public:
             adjl[u].push_back(v);
         }
         //===========================dfs===========================
-        //TC: same as dfs
-        //SC: O(2N) or O(3N) if we count the ans array too or O(4N) if we count recursive stack space too. both will be simplified to O(N)
-        // vector<bool> visited(V, false);
-        // vector<int> storage;
-        //or we can use stack
+        vector<bool> vis(V, false);
+        vector<int> storage;
+        vector<bool> pathvis(V,false);
+        // or we can use stack
         // stack<int>s;
         
         
-        // for(int i=0; i<V;i++){
-        //     if(!visited[i]){
-        //         dfs(i, adjl, visited, storage);
-        //     }
-        // }
-        // vector<int> ans;
-        // for(int i=storage.size()-1; i>=0; i--){
-        //     ans.push_back(storage[i]);
-        // }
-        // return ans;
+        for(int i=0; i<V;i++){
+            if(!vis[i]){
+                if(dfs(i, adjl, vis, pathvis, storage)) return {};
+            }
+        }
+        vector<int> ans;
+        for(int i=storage.size()-1; i>=0; i--){
+            ans.push_back(storage[i]);
+        }
+        return ans;
         
         //=============================kahn==========================
         //TC: same as bfs, coz we are traversing every node and then checking their neighbors
         //SC: O(2N) q and ans
-        queue<int> q;
-        vector<int> ans;
-        for(int i=0; i<V; i++){
-            if(indegree[i]==0) q.push(i);
-        }
+        // queue<int> q;
+        // vector<int> ans;
+        // for(int i=0; i<V; i++){
+        //     if(indegree[i]==0) q.push(i);
+        // }
         
-        while(!q.empty()){
-            int ele=q.front(); q.pop();
-            ans.push_back(ele);
-            for(int neighbor: adjl[ele]){
-                indegree[neighbor]--;
-                if(indegree[neighbor]==0) q.push(neighbor);
-            }
-        }
-        if(ans.size()<V)return {};
-        return ans;
+        // while(!q.empty()){
+        //     int ele=q.front(); q.pop();
+        //     ans.push_back(ele);
+        //     for(int neighbor: adjl[ele]){
+        //         indegree[neighbor]--;
+        //         if(indegree[neighbor]==0) q.push(neighbor);
+        //     }
+        // }
+        // if(ans.size()<V)return {};
+        // return ans;
     }
 };
