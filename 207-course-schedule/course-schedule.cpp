7481a8f19cb4a743632f578a5f879417b1763e31
Abhinav Arya <abhi9arya8@gmail.com>
2025-05-25 15:02:34 +0530

    Time: 3 ms (80.48%) | Memory: 20.9 MB (15.19%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,3 +1,18 @@
+bool dfs(int node, vector<vector<int>>&adjl, 
+vector<bool>&vis, vector<bool>& pathvis){
+    vis[node]=true;
+    pathvis[node]=true;
+    for(int neighbor: adjl[node]){
+        if(!vis[neighbor]){
+            if (dfs(neighbor, adjl, vis, pathvis)) return true;
+            //return dfs(neighbor, adjl, vis, pathvis);//can't return directly coz agar ek neighbor pe false hoga then we can't say ki har neighbor pe false hoga
+            //but agar ek neighbor pe bhi true hoga then toh pure graph ke liye true hoga
+        }
+        if(pathvis[neighbor]) return true;
+    }
+    pathvis[node]=false;
+    return false;
+}
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -13,42 +28,34 @@ public:
             // For undirected graph, also do:
             // adjl[v].push_back(u);
         }
-        //==============================dfs============================
-        // vector<vector<int>> adjMatrix(V, vector<int>(V, 0));
-        // for (auto &edge : edges) {
-        //     int u = edge[0];
-        //     int v = edge[1];
-        //     adjMatrix[u][v] = 1;
-        //     // For undirected graph, also do:
-        //     // adjMatrix[v][u] = 1;
-        // }
-        // vector<bool> vis(V,false);
-        // vector<bool> pathvis(V,false);
-        // for(int i=0; i<V;i++){
-        //     if(!vis[i]){
-        //         if(dfs(i,adjl,vis,pathvis)) return true;  
-        //     }
-        // }
-        // return false;
-        
-        //==========================kahns==========================
-        queue<int> q;
-        for(int i=0; i<V; i++){
-            if(indegree[i]==0){
-                q.push(i);
-            } 
-        }
-        int count=0;
-        while(!q.empty()){
-            int ele=q.front(); q.pop();
-            //vis[ele]=true;
-            count++;
-            for(int neighbor: adjl[ele]){
-                indegree[neighbor]--;
-                if(indegree[neighbor]==0) q.push(neighbor);
+        // ==============================dfs============================
+        vector<bool> vis(V,false);
+        vector<bool> pathvis(V,false);
+        for(int i=0; i<V;i++){
+            if(!vis[i]){
+                if(dfs(i,adjl,vis,pathvis)) return false;  
             }
         }
-        if(count<V)return false; //cycle=> cant finish tasks
-        return true;//no cycle=> can finish the tasks
+        return true;
+        
+        //==========================kahns==========================
+        // queue<int> q;
+        // for(int i=0; i<V; i++){
+        //     if(indegree[i]==0){
+        //         q.push(i);
+        //     } 
+        // }
+        // int count=0;
+        // while(!q.empty()){
+        //     int ele=q.front(); q.pop();
+        //     //vis[ele]=true;
+        //     count++;
+        //     for(int neighbor: adjl[ele]){
+        //         indegree[neighbor]--;
+        //         if(indegree[neighbor]==0) q.push(neighbor);
+        //     }
+        // }
+        // if(count<V)return false; //cycle=> cant finish tasks
+        // return true;//no cycle=> can finish the tasks
     }
 };
