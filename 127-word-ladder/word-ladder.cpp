51cd465dc656e9e22de2cce9e2531693ff5bc906
Abhinav Arya <abhi9arya8@gmail.com>
2025-05-30 05:57:43 +0530

    Time: 327 ms (33.61%) | Memory: 21.8 MB (34.34%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -7,7 +7,8 @@ public:
             mp[word]=1;
         }
         q.push({beginWord,0});
-        mp[beginWord]=2;
+        // mp[beginWord]=2;//beginword will not even be in list, so this step is unnecessary
+        if (mp.find(beginWord) != mp.end()) mp.erase(beginWord);  // optional: skip inserting if it already exists // mark as visited by removing it
         int ans=0;
         while(!q.empty()){
             pair<string,int> p = q.front(); q.pop();
@@ -19,11 +20,18 @@ public:
                 string neighbor=node;
                 for(int j=0; j<26;j++){
                     neighbor[i]='a'+j;
-                    if(mp[neighbor]==1){
-                        mp[neighbor]=2;
+                    // if(mp[neighbor]==1){
+                    //     mp[neighbor]=2;//mark as visited
+                    //     int dist_neigh=distance+1;
+                    //     if(neighbor==endWord) return dist_neigh+1;
+                    //     q.push({neighbor,dist_neigh});
+                    // }
+                    //better map
+                    if (mp.find(neighbor) != mp.end()) {
+                        mp.erase(neighbor); // mark as visited by deleting it
                         int dist_neigh=distance+1;
                         if(neighbor==endWord) return dist_neigh+1;
-                        q.push({neighbor,distance+1});
+                        q.push({neighbor, dist_neigh});
                     }
                 }
             }
