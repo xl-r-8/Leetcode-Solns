96b1e10106a2cd904b44a82e106a5f9c00a8ce63
Abhinav Arya <abhi9arya8@gmail.com>
2025-06-03 02:31:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (41.29%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,26 +1,40 @@
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int count=0;
+//         int n=nums.size();
+//         int i=0; int j=n-1;
+//         // if(nums.size()==0) return 0;
+//         while(i<=j){
+//             while(j>=0 and nums[j]==val){
+//                 count++;
+//                 j--;
+//             }
+//             while(i<n and nums[i]!=val){
+//                 i++;
+//             }
+//             if(i>j or i>=n or j<0){
+//                 break;
+//             }
+//             count++;//nums[i]==val
+//             nums[i]=nums[j];
+//             i++; j--;
+//         }
+//         // cout<<"count:"<<count<<endl;
+//         return n-count;
+//     }
+// };
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int count=0;
-        int n=nums.size();
-        int i=0; int j=n-1;
-        // if(nums.size()==0) return 0;
-        while(i<=j){
-            while(j>=0 and nums[j]==val){
-                count++;
-                j--;
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
             }
-            while(i<n and nums[i]!=val){
-                i++;
-            }
-            if(i>j or i>=n or j<0){
-                break;
-            }
-            count++;//nums[i]==val
-            nums[i]=nums[j];
-            i++; j--;
         }
-        // cout<<"count:"<<count<<endl;
-        return n-count;
+        return index;
     }
 };
