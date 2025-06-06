4d1cfc2564aa07f1b118fe15ef43c2c3efe8c1f6
Abhinav Arya <abhi9arya8@gmail.com>
2025-06-07 04:17:59 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (23.94%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,12 +1,31 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+//         unordered_map<int,int> mp;
+//         int ans=0;
+//         for(int ele:nums){
+//             mp[ele]++;
+//             if(mp[ele]>n/2)return ele;
+//         }
+//         return ans;
+//     }
+// };
+
+//better soln: O(1) space complexity
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int> mp;
-        int ans=0;
-        for(int ele:nums){
-            mp[ele]++;
-            if(mp[ele]>n/2)return ele;
+        int freq = 0, ans;
+        for(int i = 0; i < nums.size(); i++) {
+            if(!freq) {
+                ans = nums[i];
+            }
+            if(ans == nums[i]) {
+                freq++;
+            } else {
+                freq--;
+            }
         }
         return ans;
     }
