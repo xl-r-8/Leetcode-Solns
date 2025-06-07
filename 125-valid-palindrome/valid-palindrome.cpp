effa232c35413b3714b7fdc6f0668e3f4a2f97ef
Abhinav Arya <abhi9arya8@gmail.com>
2025-06-08 05:18:29 +0530

    Time: 3 ms (31.39%) | Memory: 10 MB (58.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,34 +1,53 @@
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int i=0; int j=s.size()-1;
+//         char ch1=' ',ch2=' ';
+//         while(i<j){
+//             if((s[i]>=65 and s[i]<=90) ){
+//                 ch1=s[i]+32;
+//             }
+//             else if((s[i]>=97 and s[i]<=122) or (s[i]>=48 and s[i]<=57)){
+//                 ch1=s[i];
+//             }
+//             else{
+//                 ch1=' ';
+//                 i++;
+//             }
+//             if((s[j]>=65 and s[j]<=90) ){
+//                 ch2=s[j]+32;
+//             }
+//             else if((s[j]>=97 and s[j]<=122)or (s[j]>=48 and s[j]<=57)){
+//                 ch2=s[j];
+//             }
+//             else{
+//                 ch2=' ';
+//                 j--;
+//             }
+//             if(ch1!=' ' and ch2!=' '){
+//                 if(ch1!=ch2) return false;
+//                 i++; j--;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+//simpler code:
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i=0; int j=s.size()-1;
-        char ch1=' ',ch2=' ';
-        while(i<j){
-            if((s[i]>=65 and s[i]<=90) ){
-                ch1=s[i]+32;
-            }
-            else if((s[i]>=97 and s[i]<=122) or (s[i]>=48 and s[i]<=57)){
-                ch1=s[i];
-            }
-            else{
-                ch1=' ';
-                i++;
-            }
-            if((s[j]>=65 and s[j]<=90) ){
-                ch2=s[j]+32;
-            }
-            else if((s[j]>=97 and s[j]<=122)or (s[j]>=48 and s[j]<=57)){
-                ch2=s[j];
-            }
-            else{
-                ch2=' ';
-                j--;
-            }
-            if(ch1!=' ' and ch2!=' '){
-                if(ch1!=ch2) return false;
-                i++; j--;
-            }
-        }
-        return true;
-    }
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
