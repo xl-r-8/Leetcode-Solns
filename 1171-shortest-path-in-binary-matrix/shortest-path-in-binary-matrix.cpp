eff805a8eeb8de8e4d08fad0d472959fa3bafbf4
Abhinav Arya <abhi9arya8@gmail.com>
2025-06-11 17:28:40 +0530

    Time: 319 ms (5.03%) | Memory: 103.9 MB (5.07%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        if(grid[0][0]!=0 or grid[n-1][n-1]!=0) return -1;
+        //Coz all the weights are equal and graph is undirected => bfs can be used
+
+        queue<vector<int>> q;
+        vector<vector<int>> dist(n, vector<int>(n,-1));
+        vector<vector<bool>> visited(n, vector<bool>(n,false));
+        q.push({0,0,1});
+        visited[0][0] = true;
+        dist[0][0]=1;
+
+        vector<vector<int>> dirns={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        while(!q.empty()){
+            vector<int> v = q.front(); q.pop();
+            int i=v[0];
+            int j=v[1];
+            int distance=v[2];
+            for(vector<int> temp : dirns){
+                int xi=i+temp[0];
+                int yj=j+temp[1];
+                if(xi<0 or xi>=n or yj<0 or yj>=n or visited[xi][yj] or grid[xi][yj]==1)continue;
+                visited[xi][yj] = true;
+                dist[xi][yj]=distance+1;
+                q.push({xi,yj,distance+1});
+                
+            }
+        }
+        return dist[n-1][n-1];
+    }
+};
