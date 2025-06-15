b72292fcf760c73bf7a18ccbdc0da1e821d4da9c
Abhinav Arya <abhi9arya8@gmail.com>
2025-06-16 04:02:32 +0530

    Time: 8 ms (61.57%) | Memory: 38.2 MB (22.37%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,44 +1,48 @@
+#define ll long long
 class Solution {
 public:
-    #define ll long long
     int countPaths(int n, vector<vector<int>>& roads) {
-
-        int mod = 1e9+7;
-        vector<vector<pair<ll, ll>>> adj(n);
-
-
-        for(auto it:roads){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
+        ll MOD=1000000007;
+        vector<vector<pair<ll,ll>>> adjl(n);
+        for (auto &edge : roads) {
+            ll u = edge[0];
+            ll v = edge[1];
+            ll wt=edge[2];
+            adjl[u].push_back({v,wt});
+            adjl[v].push_back({u,wt});
         }
 
         priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
-        vector<ll>dist(n,1e18);
-        vector<ll>ways(n,0);
-
+        vector<ll> dist(n,-1);
+        vector<ll> paths(n, 0);
         pq.push({0,0});
         dist[0]=0;
-        ways[0]=1;
+        paths[0]=1;
 
         while(!pq.empty()){
-            ll node=pq.top().second;
-            ll currdist=pq.top().first;
+            // vector<ll> ele=pq.top(); 
+            ll u=pq.top().second;
+            ll dist_u=pq.top().first;
             pq.pop();
-
-            for(auto it:adj[node]){
-                ll d=it.second;
-                ll adjNode=it.first;
-
-                if(currdist+d<dist[adjNode]){
-                    dist[adjNode]=currdist+d;
-                    ways[adjNode]=ways[node];
-                    pq.push({currdist+d,adjNode});
-                    
-                }else if(currdist+d==dist[adjNode]){
-                    ways[adjNode]=(ways[adjNode]+ways[node])%mod;
+            // cout<<"Node: "<<endl;
+            // cout<<u<<" "<<paths_u<<" "<<dist[u]<<endl;
+            // cout<<"Neighbors: "<<endl;
+            for(pair<ll,ll> p: adjl[u]){
+                ll v=p.first;
+                ll wt=p.second;
+                ll dist_v=(dist_u+wt);
+                if(dist[v]==-1 or dist_v<dist[v]){
+                    dist[v]=dist_v;
+                    paths[v]=paths[u]%MOD;
+                    // cout<<v<<" "<<paths[v]<<" "<<dist_v<<endl;
+                    pq.push({dist_v,v});
+                }
+                else if(dist_v==dist[v]){
+                    paths[v]=(paths[v]+paths[u])%MOD;
+                    // cout<<v<<" "<<paths[v]<<" "<<dist_v<<endl;
                 }
             }
         }
-        return ways[n-1]%mod;
+        return paths[n-1]%MOD;
     }
 };
