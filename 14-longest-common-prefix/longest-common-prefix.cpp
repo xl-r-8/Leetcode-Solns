94d3e73719f6e362c2709d396b63c847ab51c1a8
xlr8 <abhi9arya8@gmail.com>
2025-06-21 03:05:55 +0530

    Time: 2 ms (14.76%) | Memory: 14.8 MB (7.41%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,16 +1,28 @@
+bool cmp(string a, string b) {
+    if (a.size() == b.size()) return a < b; // lexicographical if lengths match
+    return a.size() < b.size(); // shorter string comes first
+}
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& v) {
-        string ans="";
-        sort(v.begin(),v.end());
-        int n=v.size();
-        string first=v[0],last=v[n-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                return ans;
+        sort(v.begin(), v.end(), cmp);
+        string first=v[0]; int sz=first.size();
+        int ind=200; //Max length of string is 200
+        for(int i=1; i<v.size(); i++){
+            string s=v[i];
+            for(int j=0; j<sz; j++){
+                if(j>ind) break;
+                if(j>=s.size()){
+                    ind=min(ind, j-1);
+                    break;
+                }
+                if(first[j]!=s[j]){
+                    ind=min(ind, j-1);//i.e. upto j-1th index both strings are same
+                    if(ind==-1) return "";
+                } 
             }
-            ans+=first[i];
         }
+        string ans=first.substr(0, ind+1); // starts at index 0, length i
         return ans;
     }
 };
