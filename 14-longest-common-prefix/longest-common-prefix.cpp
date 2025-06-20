1952658a2f6e71eac3ac541abaf47f787c775dee
xlr8 <abhi9arya8@gmail.com>
2025-06-21 03:16:40 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (33.56%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,23 +5,25 @@ bool cmp(string a, string b) {
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& v) {
-        sort(v.begin(), v.end());
-        string first=v[0]; int sz=first.size();
+        // sort(v.begin(), v.end(),cmp);//compare by size first and then lexicographically
+        sort(v.begin(), v.end());//compare lexicographically
+        int n=v.size();
+        string first=v[0]; 
         int ind=200; //Max length of string is 200
-        for(int i=1; i<v.size(); i++){
-            string s=v[i];
-            for(int j=0; j<sz; j++){
-                if(j>ind) break;
-                if(j>=s.size()){
-                    ind=min(ind, j-1);
-                    break;
-                }
-                if(first[j]!=s[j]){
-                    ind=min(ind, j-1);//i.e. upto j-1th index both strings are same
-                    if(ind==-1) return "";
-                } 
+        // for(int i=v.size()-1; i>=1; i--){ //start from the last, when comparing lexicographically to get the max difference and hence the min prefix, yk what compare with last only coz that will only differ the most hence min prefix can be obtained by finding whats common b/w first and last
+        string last=v[n-1];
+        for(int j=0; j<first.size(); j++){
+            if(j>ind) break;
+            if(j>=last.size()){
+                ind=min(ind, j-1);
+                break;
             }
+            if(first[j]!=last[j]){
+                ind=min(ind, j-1);//i.e. upto j-1th index both strings are same
+                if(ind==-1) break;
+            } 
         }
+
         string ans=first.substr(0, ind+1); // starts at index 0, length i
         return ans;
     }
