4a2ca1a1970fd94d78677062637e362f1a5b67cc
xlr8 <abhi9arya8@gmail.com>
2025-06-21 03:42:28 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (67.50%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -9,17 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+bool soln(TreeNode* node, int targetSum, int sum){
+    if (!node) return false;
+    sum+=node->val;
+    if (!node->left && !node->right) {//i.e. its a leaf node
+        if(sum==targetSum) return true;
+        return false;
+    }
+
+    bool soln1= soln(node->left, targetSum, sum);//soln from left child
+    bool soln2= soln(node->right, targetSum, sum);//soln from right child
+
+    if(soln1 or soln2) return true; //if soln exists from any child then a path exists, otherwise it does not exist for the current node
+    return false;
+}
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (!root) return false;
-
-        if (!root->left && !root->right) {
-            return targetSum - root->val == 0;
-        }
-
-        targetSum -= root->val;
-
-        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);        
+        return soln(root, targetSum,0);    
     }
 };
