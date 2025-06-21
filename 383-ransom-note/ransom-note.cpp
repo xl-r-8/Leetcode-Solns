369629728eb10161eb6c83a3e6bbf0f93515e44e
xlr8 <abhi9arya8@gmail.com>
2025-06-21 23:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (53.10%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq1(26,0), freq2(26,0);
+        for(char ch:ransomNote){
+            freq1[ch-'a']++;
+        }
+        for(char ch:magazine){
+            freq2[ch-'a']++;
+        }
+        for(int i=0; i<26;i++){
+            if(freq1[i]>freq2[i]) return false;
+        }
+        return true;
+    }
+};
