43de68878fbbb7b3ee2d33d730447e6a3a2ad301
xlr8 <abhi9arya8@gmail.com>
2025-06-30 23:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (63.94%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,30 @@
-int pnotp(int i, vector<int>&v, vector<int>&dp){
-    if(i>=v.size()){//i>=n
-        return 0;
-    } 
-    if(dp[i]>=0) return dp[i];
-    //not pick
-    int val1=pnotp(i+1,v,dp);
-    //pick
-    int val2=pnotp(i+2,v,dp)+v[i];
-    dp[i]=max(val1,val2);
-    return dp[i];
-}
+// int pnotp(int i, vector<int>&v, vector<int>&dp){
+//     if(i>=v.size()){//i>=n
+//         return 0;
+//     } 
+//     if(dp[i]>=0) return dp[i];
+//     //not pick
+//     int val1=pnotp(i+1,v,dp);
+//     //pick
+//     int val2=pnotp(i+2,v,dp)+v[i];
+//     dp[i]=max(val1,val2);
+//     return dp[i];
+// }
 
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return pnotp(0, nums,dp);
+        // vector<int> dp(nums.size(),-1);
+        // return pnotp(0, nums,dp);
+
+        int n=nums.size();
+        if(n<=1) return nums[0];
+        vector<int> dp(n,-1);
+        dp[n-1]=nums[n-1];
+        dp[n-2]=max(0+dp[n-1],nums[n-2]);
+        for(int i=n-3;i>=0;i--){
+            dp[i]=max(0+dp[i+1],nums[i]+dp[i+2]);
+        }
+        return dp[0];
     }
 };
