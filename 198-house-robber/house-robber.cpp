0b0b1f0e291e9a59a121b26ab4ab6ba1ad99ad9d
xlr8 <abhi9arya8@gmail.com>
2025-06-30 23:29:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (72.54%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,3 +1,5 @@
+//recursion but not correct approach for memoization
+
 // int pnotp(int i, vector<int>&v, vector<int>&dp){
 //     if(i>=v.size()){//i>=n
 //         return 0;
@@ -14,17 +16,33 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        //Memoization
         // vector<int> dp(nums.size(),-1);
         // return pnotp(0, nums,dp);
 
+
+        //tabulation
         int n=nums.size();
         if(n<=1) return nums[0];
-        vector<int> dp(n,-1);
-        dp[n-1]=nums[n-1];
-        dp[n-2]=max(0+dp[n-1],nums[n-2]);
+        // vector<int> dp(n,-1);
+        // dp[n-1]=nums[n-1];
+        // dp[n-2]=max(0+dp[n-1],nums[n-2]);
+        // for(int i=n-3;i>=0;i--){
+        //     dp[i]=max(0+dp[i+1],nums[i]+dp[i+2]);
+        // }
+        // return dp[0];
+
+        //optimized tabulation
+        int pop=nums[n-1];
+        int p=max(0+pop,nums[n-2]);
         for(int i=n-3;i>=0;i--){
-            dp[i]=max(0+dp[i+1],nums[i]+dp[i+2]);
+            int temp=max(0+p,nums[i]+pop);
+            pop=p;
+            p=temp;
         }
-        return dp[0];
+        return p;
+
+
+
     }
 };
