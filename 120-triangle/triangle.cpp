ecd63890a9930defedc7dfb4f21984f5a9492288
xlr8 <abhi9arya8@gmail.com>
2025-07-14 22:56:13 +0530

    Time: 4 ms (7.88%) | Memory: 13.5 MB (5.87%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<int> dp(n+1,0);
+
+
+
+        //Tabulation
+        vector<vector<int>> dp(n+1, vector<int> (n+1,0));
         for(int i=n-1; i>=0; i--){
-            vector<int> temp(n+1,0);
+            // vector<int> temp(n+1,0);
             vector<int> v=triangle[i];
             for(int j=0; j<v.size();j++){
-                temp[j]=v[j] + min(dp[j],dp[j+1]);
+                // temp[j]=v[j] + min(dp[j],dp[j+1]);
+                dp[i][j]=v[j] + min(dp[i+1][j],dp[i+1][j+1]);
             }
-            dp=temp;
         }
-        return dp[0];
+        return dp[0][0];
+        //optimised
+        // vector<int> dp(n+1,0);
+        // for(int i=n-1; i>=0; i--){
+        //     vector<int> temp(n+1,0);
+        //     vector<int> v=triangle[i];
+        //     for(int j=0; j<v.size();j++){
+        //         temp[j]=v[j] + min(dp[j],dp[j+1]);
+        //     }
+        //     dp=temp;
+        // }
+        // return dp[0];
     }
 };
