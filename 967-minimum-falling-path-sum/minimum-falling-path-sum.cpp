5dcf0bac1080a40dc18950c418a1b34168e1b401
xlr8 <abhi9arya8@gmail.com>
2025-07-17 00:24:19 +0530

    Time: 140 ms (5.78%) | Memory: 156.4 MB (5.89%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,30 +1,57 @@
+int rec(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+    int n=matrix.size();
+    if(i==0 and j==-1){
+        int ans=1e9;
+        vector<int> u=matrix[i];
+        for(int k=0; k<n;k++){
+            if(n==1) dp[0][k]=u[k];
+            else dp[0][k]=u[k] + min({rec(i+1,k-1,matrix,dp),rec(i+1,k,matrix,dp), rec(i+1,k+1,matrix,dp)});
+            ans=min(ans,dp[0][k]);
+        }
+        return ans;
+    }
+    if(j>=n or j<0) return 1e9;
+    vector<int> v=matrix[i];
+    if(i==n-1) return v[j];
+    if(dp[i][j]!=1e9)return dp[i][j];
+
+    dp[i][j]=v[j] + min({rec(i+1,j-1,matrix,dp),rec(i+1,j,matrix,dp), rec(i+1,j+1,matrix,dp)});
+
+    return dp[i][j];
+
+}
+
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         //why 1e9 works as the max? and why dont we need INT_MAX?
         //coz max sum that can be=100*100=10^4, so anything >= 10^4 works
+        
+        vector<vector<int>> dp(n,vector<int>(n,1e9));
+        return rec(0,-1,matrix,dp);
+
 
         //Tabulation
-        vector<vector<int>> dp(n,vector<int>(n,1e9));
-        for(int i=n-1; i>=0; i--){
-            vector<int> v=matrix[i];
-            for(int j=0; j<v.size();j++){
-                if(i==n-1){
-                    dp[i][j]=v[j];
-                    continue;
-                }
-                int ld=(j-1>=0)? dp[i+1][j-1]: 1e9;
-                int dd= dp[i+1][j];
-                int rd=(j+1<n)? dp[i+1][j+1]:1e9;
-                dp[i][j]=v[j] + min({ld,dd,rd});
-            }
-        }
-        int ans=1e9;
-        for(int i=0; i<n;i++){
-            ans=min(dp[0][i],ans);
-        }
-        return ans;
+        // vector<vector<int>> dp(n,vector<int>(n,1e9));
+        // for(int i=n-1; i>=0; i--){
+        //     vector<int> v=matrix[i];
+        //     for(int j=0; j<v.size();j++){
+        //         if(i==n-1){
+        //             dp[i][j]=v[j];
+        //             continue;
+        //         }
+        //         int ld=(j-1>=0)? dp[i+1][j-1]: 1e9;
+        //         int dd= dp[i+1][j];
+        //         int rd=(j+1<n)? dp[i+1][j+1]:1e9;
+        //         dp[i][j]=v[j] + min({ld,dd,rd});
+        //     }
+        // }
+        // int ans=1e9;
+        // for(int i=0; i<n;i++){
+        //     ans=min(dp[0][i],ans);
+        // }
+        // return ans;
         //optimised
         // vector<int> dp(n,0);
         // for(int i=n-1; i>=0; i--){
