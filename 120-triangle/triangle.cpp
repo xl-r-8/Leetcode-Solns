cbb88f4e3149064f651b8dff7f3110ad08f0104f
xlr8 <abhi9arya8@gmail.com>
2025-07-16 23:56:51 +0530

    Time: 32 ms (3.71%) | Memory: 49.7 MB (5.91%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,6 +1,6 @@
 int rec(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp){
     int n=triangle.size();
-    if(i>=n or j>i) return 0;
+    // if(i>=n or j>i) return 0;
     vector<int> v=triangle[i];
     if(i==n-1) return v[j];
     if(dp[i][j]!=1e9)return dp[i][j];
@@ -19,10 +19,12 @@ public:
         //M1: dp of size (n)^2
         // vector<vector<int>> dp(n, vector<int> (n,1e9));
         //M2: dp of size(1+2+...+n = n(n+1)/2)
-        vector<vector<int>> dp;
-        for(int i=1; i<=n; i++){
-            vector<int> v(i,1e9);
-            dp.push_back(v);
+        vector<vector<int>> dp(n); //if you initialize like this then dp stores n empty vectors rn, so in the next step you should fill up these vectors like dp[i]=v;
+        // vector<vector<int>> dp; //if you initialize like this then dp is empty rn so in the next step you should push vectors in it, dp.push_back(v);
+        for(int i=0; i<n; i++){
+            vector<int> v(i+1,1e9); //coz row 0 has 1 ele, and so on
+            dp[i]=v;
+            // dp.push_back(v);
         }
         return rec(0,0,triangle,dp);
 
