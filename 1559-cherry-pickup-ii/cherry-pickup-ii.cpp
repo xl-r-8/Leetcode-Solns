93d38e50d0901ba24c505bb1e5a3c3124fda8167
xlr8 <abhi9arya8@gmail.com>
2025-07-21 00:47:38 +0530

    Time: 163 ms (5.02%) | Memory: 40.7 MB (5.02%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,20 +1,23 @@
 int rec(int i, int j1, int j2, vector<vector<vector<int>>>&dp,vector<vector<int>>& grid){
     int m=grid.size(); int n=grid[0].size();
-    if(i>=m or j1<0 or j1>=n or j2<0 or j2>=n) return -1; //if any bot is out of bounds then cant pick any cherries
+    if(i>=m or j1<0 or j1>=n or j2<0 or j2>=n) return -1; //if any bot is out of bounds then cant pick any cherries,, why return -1? why not 0, coz 0 can be returned by a valid path and then if an invalid path or a path that doesnt even exist returns 0 then how do we check the validity of a path? eg: in this condition :if(dp[i][j1][j2]!=-1) return dp[i][j1][j2]; if we replace -1 with 0 then all the valid paths which return 0 have to re calculated and re computed by recursion again and again coz you are just ignoring valid paths with 0 cherry pick ups. but we can ignore the ones where we pick up -1 cherries, coz that would always be invalid.
+
     if(i==m-1){
         if(j1==j2) return grid[i][j1];
         else return grid[i][j1]+grid[i][j2];
     }
     if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
     vector<int> ind={-1,0,1};
+    int maxval=0;
     for(int a: ind){
         for(int b:ind){
-            int addition=0;
-            if(j1==j2)addition=grid[i][j1];
-            else addition=grid[i][j1]+grid[i][j2];
-            dp[i][j1][j2]= max(dp[i][j1][j2], addition+rec(i+1, j1+a,j2+b,dp,grid));
+            maxval=max(maxval,rec(i+1, j1+a,j2+b,dp,grid));
         }
     }
+    int addition=0;
+    if(j1==j2)addition=grid[i][j1];
+    else addition=grid[i][j1]+grid[i][j2];
+    dp[i][j1][j2]= addition + maxval;
     return dp[i][j1][j2];    
 }
 
