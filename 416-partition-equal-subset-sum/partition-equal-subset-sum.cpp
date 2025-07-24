c71285d19f12aa15033f615987f018ba4c6f3a2b
xlr8 <abhi9arya8@gmail.com>
2025-07-25 00:57:26 +0530

    Time: 99 ms (74.24%) | Memory: 15.6 MB (77.02%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        //dp, recursion
+        //total sum must be even
+        long long sum=0;
+        for(int ele: nums){
+            sum+=ele;
+       }
+       if(sum%2!=0)return false;   
+       int k= sum/2; // if you can just find a subset that can make sum/2, then the other elements left in the set will also have to add up to sum/2 coz totalsum=sum
+//{a1,a2,a3,...,an},where a1+a2+...+an=2k=syn since it is a set, say a1+a2+a3+...+ai=k without the loss of generality therefore ai+...+an=k, and hence we can partition the set into 2 subsets of equal sum
+        int n=nums.size();
+       vector<bool> dp(k+1,false);
+        dp[0]=true;
+        for(int i=n-1; i>=0; i--){
+            vector<bool> temp(k+1,false);
+            temp[0]=true;
+            for(int j=1; j<=k; j++){
+                bool notp=dp[j];
+                bool p=(j-nums[i]>=0)? dp[j-nums[i]] : false;
+                temp[j]=notp or p;
+                if(temp[k]) return true;
+            }
+            dp=temp;
+        }
+        return dp[k];
+    }
+};
