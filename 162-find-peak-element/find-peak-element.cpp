1fbe0c5d3b866081561a5a2a2a27265b4fd1bd6d
xlr8 <abhi9arya8@gmail.com>
2025-08-22 15:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.97%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -3,13 +3,15 @@ public:
     int findPeakElement(vector<int>& nums) {
         //O(log n) in n sized storages => binary search
         //M1
-        int n=nums.size();
-        if(n==1) return 0;
-        if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
-        for(int i=1; i<n-1; i++){
-            if(nums[i]>=nums[i-1] and nums[i]>=nums[i+1]) return i;
-        }
-        return -1;
+        // int n=nums.size();
+        // if(n==1) return 0;
+        // if(nums[0]>nums[1]) return 0;
+        // if(nums[n-1]>nums[n-2]) return n-1;
+        // for(int i=1; i<n-1; i++){
+        //     if(nums[i]>=nums[i-1] and nums[i]>=nums[i+1]) return i;
+        // }
+        // return -1;
+
+        return max_element(nums.begin(),nums.end()) - nums.begin();
     }
 };
