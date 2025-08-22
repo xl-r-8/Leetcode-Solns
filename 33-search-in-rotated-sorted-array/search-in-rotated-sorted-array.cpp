a4d9532a7087c21702a6a891019ec9b1f806df3c
xlr8 <abhi9arya8@gmail.com>
2025-08-22 15:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (94.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        //M1: normal search
+        // int n=nums.size();
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]==target) return i;
+        // }
+        // return -1;
+
+        //M2
         int n=nums.size();
-        for(int i=0; i<n; i++){
-            if(nums[i]==target) return i;
+        auto it=min_element(nums.begin(),nums.end());
+        int offset= it - nums.begin();// index of min ele
+        int low=0; int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int ind = (mid + offset) % n;
+            if(nums[ind] == target) return ind;
+            else if( nums[ind] < target) low = mid + 1;
+            else high = mid - 1;
         }
         return -1;
     }
