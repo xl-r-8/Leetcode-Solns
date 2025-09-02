dad8d43b8afeb64412d87a782d69afc1bf5356e8
xlr8 <abhi9arya8@gmail.com>
2025-09-03 02:45:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (30.43%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,13 +1,36 @@
+int lb2(vector<int>& arr, int target){
+    int n = arr.size();
+    int low = 0, high = n-1, ans=n;
+    while(low <= high) {
+        int mid = low + (high-low)/2;
+        if(target <= arr[mid]){
+            ans=mid;
+            high=mid-1;
+        }
+        else low = mid + 1;
+    }
+    return ans;
+}
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        //M1: direct soln
         int n=nums.size();
+        
+        //M1: direct soln
+        // for(int i=1; i<=n; i++){
+        //     int x=i; int count=0;
+        //     for(int j=0; j<n;j++){
+        //         if(nums[j]>=x)count++;
+        //     }
+        //     if(count==x) return x;
+        // }
+        // return -1;
+
+        //M2: bs
+        sort(nums.begin(),nums.end());
         for(int i=1; i<=n; i++){
-            int x=i; int count=0;
-            for(int j=0; j<n;j++){
-                if(nums[j]>=x)count++;
-            }
+            int x=i; 
+            int count=n-lb2(nums, x);
             if(count==x) return x;
         }
         return -1;
