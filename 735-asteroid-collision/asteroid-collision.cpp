6c2fd69b80123eead59cd83aedb97873f9711256
xlr8 <abhi9arya8@gmail.com>
2025-09-24 09:26:06 +0530

    Time: 3 ms (29.78%) | Memory: 22.1 MB (41.62%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        std::stack<int> stack;
-
-        for (int a : asteroids) {
-            if (a > 0) {
-                stack.push(a);
-            } else {
-                while (!stack.empty() && stack.top() > 0 && stack.top() < -a) {
-                    stack.pop();
-                }
-
-                if (stack.empty() || stack.top() < 0) {
-                    stack.push(a);
-                }
-
-                if (!stack.empty() && stack.top() == -a) {
-                    stack.pop();
+        stack<int> s;
+        int n=asteroids.size();
+        for(int i=0; i<n; i++){
+            if(asteroids[i]>=0) s.push(asteroids[i]);
+            else{
+                if(s.empty()){
+                    s.push(asteroids[i]);
+                    continue;
+                } 
+                while(!s.empty()){
+                    int ele= s.top();
+                    int negele=asteroids[i];
+                    if(ele>=0){
+                        if(negele+ele<0){
+                            s.pop();
+                            if(s.empty()){
+                                s.push(negele);
+                                break;
+                            }
+                        }
+                        else if(negele+ele==0){
+                            s.pop();
+                            break;
+                        }
+                        else break;
+                    }
+                    else{
+                        s.push(negele);
+                        break;
+                    }
                 }
             }
         }
 
-        std::vector<int> res(stack.size());
-        int i = stack.size() - 1;
-
-        while (!stack.empty()) {
-            res[i--] = stack.top();
-            stack.pop();
+        vector<int> ans;
+        while(!s.empty()){
+            ans.push_back(s.top()); s.pop();
         }
-
-        return res;        
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
