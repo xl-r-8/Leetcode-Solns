1835228b2821697bc0a5440c9a1ed1a744fc3b1e
xlr8 <abhi9arya8@gmail.com>
2025-10-19 14:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (13.43%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,17 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-void inorder(TreeNode* root,vector<int>& storage){
-    if(root==nullptr)return;
-    inorder(root->left,storage);
-    storage.push_back(root->val);
-    inorder(root->right,storage);
-}
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        inorder(root,ans);
-        return ans;
+    //vector return karega jisme inorder will be stored and hum tree ka root pass karenge as arg to perform inorder
+vector<int>inorderTraversal(TreeNode* root){
+vector<int>result;// to store and return inorder traversal
+if(root==NULL)return result;//return empty vector if root is null ptr i.e tree is empty
+//otherwise create a stack since this is dfs and we will go depth wise
+stack<TreeNode*>st;//stack me treenodes push kar rahe to wohi type of stack
+//go as left as possible first
+TreeNode* node=root;
+while(true){
+    if(node!=nullptr){
+        st.push(node);
+        node=node->left;
     }
-};
+    //ab jab left me jate jate dead end aagya to
+    else{
+    if(st.empty()==true)break;//stack bhi khali hogya
+    //ab stack me tos pe leftmost aadmi hai to wahi to inorder me chahiye
+    //inorder is left->root>right
+    //so push karo result me tos ko
+    node=st.top();
+    st.pop();
+    result.push_back(node->val);
+    // now move as right as possible
+    node=node->right;
+    }
+}
+return result;
+}
+        
+    };
