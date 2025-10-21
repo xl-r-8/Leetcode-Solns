0901be82f83186e6d69144e92b6bd0512d533547
xlr8 <abhi9arya8@gmail.com>
2025-10-21 18:30:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (47.33%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -3,20 +3,45 @@ public:
     int maxScore(vector<int>& cardPoints, int k) {
         int n=cardPoints.size();
 
+        //M1
+        // int sum=0;
+        // for(int i=0; i<k; i++){
+        //     sum+=cardPoints[i];
+        // }
+        
+        // int mxsum=sum;
+        // int l=k-1, r=n-1; 
+        // while(l>=0){
+        //     sum-=cardPoints[l];
+        //     l--;
+        //     sum+=cardPoints[r];
+        //     r--;
+        //     mxsum=max(mxsum,sum);
+        // }
+        // return mxsum;
+
+        //M2
+        int total=0;
+        for(int i=0; i<n; i++){
+            total+=cardPoints[i];
+        }
+
+        int mnsum=total;
         int sum=0;
-        for(int i=0; i<k; i++){
+        for(int i=0; i<n-k; i++){
             sum+=cardPoints[i];
         }
-        
-        int mxsum=sum;
-        int l=k-1, r=n-1; 
-        while(l>=0){
+        mnsum=min(mnsum, sum);
+
+        int l=0, r=n-k;
+        while(r<n){
             sum-=cardPoints[l];
-            l--;
+            l++;
             sum+=cardPoints[r];
-            r--;
-            mxsum=max(mxsum,sum);
+            r++;
+            mnsum=min(mnsum, sum);
         }
-        return mxsum;
+        
+        return total-mnsum;
     }
 };
