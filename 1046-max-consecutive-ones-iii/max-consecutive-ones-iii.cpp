7c2700b898229f4b702261c7402347d491b5b95b
xlr8 <abhi9arya8@gmail.com>
2025-10-22 13:17:15 +0530

    Time: 0 ms (100.00%) | Memory: 69.4 MB (91.26%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -40,17 +40,32 @@ public:
         // return maxlen;  
 
         //M2 ---> while loop approach
+        // int l=0, r=0;
+        // int count=0, maxlen=0;
+        // while(r<n){
+        //     if(nums[r]==0)count++;
+        //     while(count>k){
+        //         if(nums[l]==0)count--;
+        //         l++;
+        //     }
+        //     maxlen=max(maxlen, r-l+1);
+        //     r++;
+        // }
+        // return maxlen;
+
+        //M3 ---> optimal
         int l=0, r=0;
         int count=0, maxlen=0;
         while(r<n){
             if(nums[r]==0)count++;
-            while(count>k){
+            if(count>k){
                 if(nums[l]==0)count--;
                 l++;
             }
-            maxlen=max(maxlen, r-l+1);
+            if(count<=k)maxlen=max(maxlen, r-l+1); //only consider its length if its a valid window
             r++;
         }
         return maxlen;
+
     }
 };
