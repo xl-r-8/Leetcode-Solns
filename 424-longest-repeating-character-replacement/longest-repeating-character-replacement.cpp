4aafc18aa6dc46d60117e5909f783dc57f8d7130
xlr8 <abhi9arya8@gmail.com>
2025-10-23 15:50:36 +0530

    Time: 3 ms (68.47%) | Memory: 10.8 MB (69.54%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        if(n==0) return 0; 
+
+        //M1: brute
+        // int maxlen=1; //atleast 1 char substring toh you will get which will have the same char for any fucking k
+        // for(int i=0; i<n; i++){
+        //     int count=0;
+        //     char ch=s[i]; 
+        //     int window=1;// size of current window
+        //     for(int j=i+1; j<n; j++){ //check on the right of ch
+        //         if(s[j]!=ch) count++;
+        //         if(count>k) break;
+        //         window++;
+        //     }
+        //     for(int j=i-1; j>=0; j--){//check on the left of ch
+        //         if(s[j]!=ch) count++;
+        //         if(count>k) break;
+        //         window++;
+        //     }
+        //     maxlen=max(maxlen, window);
+        // }
+        // return maxlen;
+
+        //M1: diff brute
+
+        //M3: optimal
+        vector<int> freq(26,0); 
+        int l=0, r=0;
+        int maxfreq=0, maxlen=0;
+
+        while(r<n){
+            char ch=s[r];
+            freq[ch-'A']++;
+            if(freq[ch-'A']>maxfreq)maxfreq=freq[ch-'A'];
+            int len=r-l+1;
+            int minchanges=len-maxfreq;
+            if(minchanges<=k){
+                maxlen=max(maxlen, len);
+            }
+            else{ //if(minchanges>k) => shrink window by 1 place
+                char ch2=s[l];
+                freq[ch2-'A']--;
+                l++;
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
