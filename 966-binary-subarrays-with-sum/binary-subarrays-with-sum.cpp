aab69636a0950c6915768caec9770ffe9635230f
xlr8 <abhi9arya8@gmail.com>
2025-10-26 11:45:23 +0530

    Time: 15 ms (34.29%) | Memory: 39.9 MB (8.33%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -22,30 +22,30 @@ public:
         int n=nums.size();
 
         //M1: optimal but SC=O(N)
-        // unordered_map<int, int> mp;
-        // vector<int> presum(n);
-        // if(goal!=0)mp[0]=1;
-        // int count=0;
-        // presum[0]=nums[0];
-        // int x=presum[0];
-        // if(x==0 and goal==0) count++;
-        // if(mp.find(x-goal)!=mp.end()){//i.e. x-k exists in mp
-        //     count+=mp[x-goal];
-        // }
-        // mp[presum[0]]++;
-        // for(int r=1; r<n; r++){
-        //     presum[r]=nums[r]+presum[r-1];
-        //     x=presum[r];
-        //     if(x==0 and goal==0) count++; //coz it is the only case when x-k=x
-        //     if(mp.find(x-goal)!=mp.end()){//i.e. x-k exists in mp
-        //         count+=mp[x-goal];
-        //     }
-        //     mp[presum[r]]++;
-        // }
-        // return count;
+        unordered_map<int, int> mp;
+        vector<int> presum(n);
+        if(goal!=0)mp[0]=1;
+        int count=0;
+        presum[0]=nums[0];
+        int x=presum[0];
+        if(x==0 and goal==0) count++;
+        if(mp.find(x-goal)!=mp.end()){//i.e. x-k exists in mp
+            count+=mp[x-goal];
+        }
+        mp[presum[0]]++;
+        for(int r=1; r<n; r++){
+            presum[r]=nums[r]+presum[r-1];
+            x=presum[r];
+            if(x==0 and goal==0) count++; //coz it is the only case when x-k=x
+            if(mp.find(x-goal)!=mp.end()){//i.e. x-k exists in mp
+                count+=mp[x-goal];
+            }
+            mp[presum[r]]++;
+        }
+        return count;
 
         //M2: most optimal, with SC=O(1)
-        return countSubarr(nums,goal)-countSubarr(nums,goal-1);
+        // return countSubarr(nums,goal)-countSubarr(nums,goal-1);
         
     }
 };
