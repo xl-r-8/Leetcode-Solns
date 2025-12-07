a741471e30764015f10764e44aa8b4b1f304ab01
xlr8 <abhi9arya8@gmail.com>
2025-12-08 03:31:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (56.87%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,22 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-int height(TreeNode* node, unordered_map<TreeNode*, int>& mp){
+
+//M1:
+// int height(TreeNode* node, unordered_map<TreeNode*, int>& mp){
+//     if(node==nullptr) return 0;
+//     return mp[node]=max(height(node->left, mp), height(node->right, mp))+1;
+// }
+// bool preorder(TreeNode* node, unordered_map<TreeNode*, int>& mp){
+//     if(node==nullptr) return true;
+//     int leftht= (node->left==nullptr)? 0 : mp[node->left]; 
+//     int rightht= (node->right==nullptr)? 0 : mp[node->right]; 
+//     if(abs(leftht-rightht)>1) return false;
+//     return (preorder(node->left, mp) and preorder(node->right, mp));
+// }
+
+//M2:
+int height2(TreeNode* node){
     if(node==nullptr) return 0;
-    return mp[node]=max(height(node->left, mp), height(node->right, mp))+1;
-}
-bool preorder(TreeNode* node, unordered_map<TreeNode*, int>& mp){
-    if(node==nullptr) return true;
-    int leftht= (node->left==nullptr)? 0 : mp[node->left]; 
-    int rightht= (node->right==nullptr)? 0 : mp[node->right]; 
-    if(abs(leftht-rightht)>1) return false;
-    return (preorder(node->left, mp) and preorder(node->right, mp));
+    int leftht=height2(node->left);
+    int rightht=height2(node->right);
+    if(abs(leftht-rightht)>1 or leftht==-1 or rightht==-1) return -1; //if(abs(leftht-rightht)>1 or leftht==-1 or rightht==-1), do we need these extra conditions?
+    return max(leftht, rightht)+1;
 }
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        unordered_map<TreeNode*,int> mp; 
-        height(root, mp);
-        return preorder(root, mp);
+        //M1:
+        // unordered_map<TreeNode*,int> mp; 
+        // height(root, mp);
+        // return preorder(root, mp);
+
+        //M2
+        if(height2(root)==-1)return false;
+        return true;
     }
 };
