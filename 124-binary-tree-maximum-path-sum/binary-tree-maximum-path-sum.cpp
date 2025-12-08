4205aee0c3faeb3a89bdd4407d54706b36db0998
xlr8 <abhi9arya8@gmail.com>
2025-12-08 15:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (47.47%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -9,18 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+int psum(TreeNode* node, int &maxps){
+    if(node==nullptr) return 0;
+    int leftp=psum(node->left, maxps);
+    int rightp=psum(node->right, maxps);
+
+    maxps=max(maxps, node->val);//only node
+    maxps=max(maxps, node->val+leftp);//node+left
+    maxps=max(maxps, node->val+rightp);//node+right
+    maxps=max(maxps, node->val+leftp+rightp);//node+left+right
+    //choose which path would be best for par(node)
+    if(max(leftp, rightp)<=0) return node-> val; //only node
+    return node->val + max(leftp, rightp);//node + max(left child of node, right child)
+}
+
 class Solution {
 public:
-int maxp(TreeNode*root,int &maxi){
-    if(root==nullptr)return 0;
-    int lh=max(0,maxp(root->left,maxi));
-    int rh=max(0,maxp(root->right,maxi));
-    maxi=max(maxi,lh+rh+root->val);
-    return root->val+max(lh,rh);
-}
     int maxPathSum(TreeNode* root) {
-    int maxps=INT_MIN;
-    maxp(root,maxps);
-    return maxps;
+        int maxps=INT_MIN;
+        psum(root, maxps);
+        return maxps;
     }
 };
