d9e77cb1c8a45ff1c089f7c9b49837c5f09bee0c
xlr8 <abhi9arya8@gmail.com>
2025-12-30 01:34:07 +0530

    Time: 61 ms (2.83%) | Memory: 98.9 MB (5.03%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,20 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-TreeNode* bst(vector<int>& preorder, int i, int mn, int mx){
+TreeNode* bst(vector<int>& preorder, int i, int mn, int mx, vector<TreeNode*>dp){
     if(i>=preorder.size()) return nullptr;
-    if(preorder[i]<=mn or preorder[i]>=mx) return bst(preorder, i+1, mn, mx); //if i+1 is not the node then the next one might be
+    if(preorder[i]<=mn or preorder[i]>=mx) return bst(preorder, i+1, mn, mx, dp); //if i+1 is not the node then the next one might be
+    if(dp[i]!=nullptr) return dp[i];
     TreeNode* node=new TreeNode(preorder[i]);
-    node->left = bst(preorder, i+1, mn, node->val);
-    node->right = bst(preorder, i+1, node->val, mx);
-    return node;
+    node->left = bst(preorder, i+1, mn, node->val, dp);
+    node->right = bst(preorder, i+1, node->val, mx, dp);
+    // dp[i]=node;
+    // return dp[i];
+
+    //or
+    return dp[i]=node;
 }
 
 
 class Solution {
 public:
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return bst(preorder, 0, INT_MIN, INT_MAX);
-        
+    TreeNode* bstFromPreorder(vector<int>& preorder){
+        int n=preorder.size();
+        vector<TreeNode*>dp(n, nullptr);
+        return bst(preorder, 0, INT_MIN, INT_MAX,dp); 
     }
 };
