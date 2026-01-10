5af7300fb0b8af9bc27d08f766e29bb79e16fcde
xlr8 <abhi9arya8@gmail.com>
2026-01-11 01:54:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (32.75%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,104 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-void in(TreeNode* node, vector<int>&ans){
-    if(node==nullptr) return;
-    in(node->left,ans);
-    ans.push_back(node->val);
-    in(node->right,ans);
-}
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        //M1: recursive soln
-        // in(root, ans);
-        // return ans;
+        TreeNode* curr=root;
+        unordered_map<TreeNode*, TreeNode*> next;
 
-        //M2: iterative soln
-        // stack<TreeNode*> st; unordered_map<TreeNode*, bool> mp;
-        // if(root!=nullptr)st.push(root);
-        // while(!st.empty()){
-        //     TreeNode* node=st.top();
-        //     if(mp.find(node)==mp.end()){ //node dne in the map
-        //         if(node->left!=nullptr){
-        //             st.push(node->left);
-        //             continue;
-        //         } 
-        //         else{
-        //             ans.push_back(node->val);
-        //             mp[node]=true;
-        //         }
-        //     }
-
-        //     //Before checking right you are sure ki root is printed so just remove it from the stack
-        //     st.pop();
-
-        //     if(node->right!=nullptr){
-        //         st.push(node->right);
-        //         continue;
-        //     } 
-        //     else{
-        //         if(st.empty())break;
-        //         node=st.top();
-        //         ans.push_back(node->val);
-        //         mp[node]=true;
-        //     }
-
-        // }
-        // return ans;
-
-        //M3: iterative better soln
-        // stack<TreeNode*>st;
-        // TreeNode* node=nullptr;
-        // if(root!=nullptr)st.push(root);
-        // while(true){
-        //     if(node==nullptr){
-        //         if(st.empty())break;
-        //         node=st.top(); st.pop();
-        //         ans.push_back(node->val);
-        //         node=node->right;
-        //     }
-        //     else{
-        //         st.push(node);
-        //         node=node->left;
-        //     }
-        // }
-
-        //M3 but correct one
-        // stack<TreeNode*> st;
-        // TreeNode* node = root;               // start at root (IMPORTANT)
-
-        // while (node != nullptr || !st.empty()) {
-        //     if (node != nullptr) {
-        //         // go left as far as possible, pushing nodes on stack
-        //         st.push(node);
-        //         node = node->left;
-        //     } else {
-        //         // left is exhausted -> visit top, then go right
-        //         node = st.top(); st.pop();
-        //         ans.push_back(node->val);
-        //         node = node->right;
-        //     }
-        // }
-        // return ans;
-
-        //M4: my way
-        stack<TreeNode*> st;
-        if(root!=nullptr)st.push(root);
-        unordered_map<TreeNode*, bool> mp;
-        while(!st.empty()){
-            TreeNode* node=st.top();
-
-            if(node->left!=nullptr and mp.find(node->left)==mp.end()){
-                st.push(node->left);
-                continue;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                ans.push_back(curr->val);
+                if(curr->right!=nullptr)curr=curr->right;
+                else{
+                    if(next.find(curr)!= next.end()) curr=next[curr];//ie when curr exists in next
+                    else curr=nullptr;
+                }
             }
             else{
-                ans.push_back(node->val); st.pop();
-                if(node->right!=nullptr) st.push(node->right);
-                // node=nullptr;
-                mp[node]=true;
+                TreeNode* node=curr->left;
+                while(node->right!=nullptr) node=node->right;
+                if(next.find(node)!= next.end()){
+                    ans.push_back(curr->val);
+                    if(curr->right!=nullptr)curr=curr->right;
+                    else{
+                        if(next.find(curr)!= next.end()) curr=next[curr];
+                        else curr=nullptr;
+                    }
+                }
+                else{
+                    next[node]=curr;
+                    curr=curr->left;
+                }
             }
         }
         return ans;
