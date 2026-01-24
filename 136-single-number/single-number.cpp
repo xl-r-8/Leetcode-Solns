bcd2d75eb29a91c0b205c56014c1bbb9c24fbe2d
xlr8 <abhi9arya8@gmail.com>
2026-01-25 04:06:34 +0530

    Time: 8 ms (17.28%) | Memory: 24.1 MB (5.37%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //m1: brute, using map
+        unordered_map<int, int> mp;
+        for(int ele: nums) mp[ele]++;
+        int ans=-1;
+        for(auto it :mp){
+            if(it.second==1){
+                ans=it.first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
