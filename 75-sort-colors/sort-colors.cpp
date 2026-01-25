0963a26b237c9b041551669b4a3a3fbab2837d90
xlr8 <abhi9arya8@gmail.com>
2026-01-26 03:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (47.10%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //M1:using map, SC=O(n)
-        unordered_map<int,int> mp;
-        for(int ele: nums){
-            mp[ele]++;
-        }
-        for(int i=0; i<nums.size();i++){
-            if(i<mp[0])nums[i]=0;
-            else if(i>=mp[0]+mp[1]) nums[i]=2;
-            else nums[i]=1;
+        int n=nums.size();
+        int p=0, q=n-1; 
+        int i=0, j=n-1;
+
+        while(i<=q or j>=p){
+            while(i<n and nums[i]<=1)i++;
+            // cout<<i<<" "<<q<<"  "<<j<<" "<<p<<endl;
+            // cout<<"before swap: ";
+            // for(int ele: nums) cout<<ele<<" ";
+            // cout<<endl;
+            if(i<=q){
+                swap(nums[i], nums[q]);
+                q--;
+            }
+            while(j>=0 and nums[j]>=1)j--;
+            if(j>=p){
+                swap(nums[j], nums[p]);
+                p++;
+            }
+            // cout<<"after swap: ";
+            // for(int ele: nums) cout<<ele<<" ";
+            // cout<<endl;
         }
     }
 };
