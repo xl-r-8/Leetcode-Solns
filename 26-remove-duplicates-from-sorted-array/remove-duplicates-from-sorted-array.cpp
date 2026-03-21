75a7d77a437fc8ddfb83d089478073601b3079ec
xlr8 <abhi9arya8@gmail.com>
2026-03-21 12:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (52.08%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() == 1) return 1;
-        int k = 1;
-        int prevind = 1; int lastele=nums[0];
-        // cout<<prevind<<":"<<nums[prevind]<<" ";
-        for(int i = 1; i < nums.size(); i++) {
-            // cout<<"i:"<<i<<" nums[i]:"<<nums[i]<<endl;
-            if(nums[i] == lastele) continue;
-            // cout<<"initially: prevind "<<prevind<<endl;
-            lastele=nums[i];
-            k++;
-            if(prevind < nums.size()) {
-                // cout<<"Replacement:\n";
-                // cout<<"nums"<<prevind+1<<":"<<nums[prevind+1]<<endl;
-                nums[prevind] = nums[i];
-                prevind++;
-                // cout<<"nums"<<prevind+1<<":"<<nums[prevind+1]<<endl;
+        int n=nums.size();
+        int si=1;//si=swapping index
+        int lastele=nums[0];
+        if(n==1)return 1;
+
+        for(int i=1; i<n; i++){
+            if(nums[i]!=lastele){
+                lastele=nums[i];
+                swap(nums[i],nums[si]);
+                si++;
             }
-            // cout<<"updated: prevind "<<prevind<<endl<<endl;
         }
-        // cout<<prevind<<":"<<nums[prevind]<<endl;
-        return k;
+        return si;
     }
 };
