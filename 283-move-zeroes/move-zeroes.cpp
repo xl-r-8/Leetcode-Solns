4ce2c2de8b7443e86c719eacf43221aa2c1f1ec2
xlr8 <abhi9arya8@gmail.com>
2026-03-26 00:50:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.26%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        int i=0; int j=0;
-        while(i<n and j<n){
-            while(i<n and nums[i]!=0) i++;
-            while(j<n and (nums[j]==0 or j<=i) ) j++; 
-            if(i<n and j<n){
-                swap(nums[i],nums[j]);
+        int i=0; int j=-1;
+        while(i<n){
+            if(nums[i]==0){
+                if(j==-1)j=i;
             }
-        }   
+            else{
+                if(j!=-1 and i>j){
+                    swap(nums[j],nums[i]);
+                    j++;
+                }
+            }
+            i++;
+        }
+        
+        //sunn rishu, you need not move j ptr coz i is already checking 0 and non 0 eles, and to check for 2 states you need max 1 num, and in this case worst TC=O(2n). instead of using j as pointer you can use j as a state and save the 1st index of 0 and just move pointer i, which will ensure TC=O(n).
+        //
     }
 };
