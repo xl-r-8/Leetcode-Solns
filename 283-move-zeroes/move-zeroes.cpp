dfef8bed9fc56c25cfdb1c9cb0aca269349400ea
xlr8 <abhi9arya8@gmail.com>
2026-03-26 00:53:05 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (81.81%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -17,6 +17,6 @@ public:
         }
         
         //sunn rishu, you need not move j ptr coz i is already checking 0 and non 0 eles, and to check for 2 states you need max 1 num, and in this case worst TC=O(2n). instead of using j as pointer you can use j as a state and save the 1st index of 0 and just move pointer i, which will ensure TC=O(n).
-        //
+        //iske notes mein suppose karna that you have k contiguous 0s, and j is the index of 1st 0. 2 cases: C1: k>1 ke liye dekhna j aur i mein swapping kab hoti hai and swap hone ke baad ptrs kaise move karte hai, watch the same for C2
     }
 };
