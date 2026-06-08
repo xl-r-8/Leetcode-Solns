67d642ae339f44bf7693bb9d3e300013cc54ba82
xlr8 <abhi9arya8@gmail.com>
2026-06-08 20:40:55 +0530

    Time: 9 ms (55.04%) | Memory: 11.8 MB (52.73%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,30 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        //M1: using visited: SC = O(n), but can we do without visited?
-        unordered_set<ListNode*> st;//cant store values in set, coz they can be repeated, but address of each node is unique or ListNode* ptr for each node is unique
-        // ListNode* temp = head;
-        // while( temp != nullptr ){
-        //     // cout<<temp->val<<endl;
-        //     if( st.find(temp) != st.end() ){
-        //         return true;
-        //     }
-        //     st.insert(temp);
-        //     temp=temp->next;
-        // }
-        // return false;
-
-        //M2: without visited: sc = o(1)
-        //weird method: but since ik the max nodes in this ques can be 10^4, so i initialize a temp ptr as head then keep on moving it(temp=temp->next) and i will keep a counter which will count the num of nodes or num of iterations
-        //All those linked list with no loop will stop before count exceeds 10^4, but those with loop will exceed it
-        //Incorrect method but a clever trick
-        int count = 0;
-        ListNode* temp = head;
-        while( temp != nullptr ){
-            count++;
-            temp = temp->next;
-            if(count>1e4) return true;
+        //M3: tortoise - rabbit method
+        if(head == nullptr or head->next == nullptr) return false;
+        ListNode* tortoise = head;
+        ListNode* rabbit = head->next;
+        while( rabbit != nullptr ){
+            if(tortoise == rabbit) return true;
+            tortoise = tortoise->next;
+            if(rabbit->next == nullptr or rabbit->next->next == nullptr) break;
+            rabbit = rabbit->next->next;
         }
         return false;
-    }
+    } 
 };
