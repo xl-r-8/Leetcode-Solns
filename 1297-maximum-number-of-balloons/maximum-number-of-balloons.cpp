193dbb24d517e639de987536144564cd52445b7f
xlr8 <abhi9arya8@gmail.com>
2026-06-22 15:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (84.03%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+
+        for(char ch: text){
+            if(ch == 'b') b++;
+            else if(ch == 'a') a++;
+            else if(ch == 'l') l++;
+            else if(ch == 'o') o++;
+            else if(ch == 'n') n++;
+        }
+        l = l/2; o = o/2;
+        int ans = min({b, a, l, o , n});
+        return ans;
+    }
+};
