88bc250b5c12251f3b2f73cf7649d4dc1fa22ed7
xlr8 <abhi9arya8@gmail.com>
2026-06-30 13:09:52 +0530

    Time: 6 ms (69.06%) | Memory: 11 MB (57.43%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -24,8 +24,18 @@ public:
             else if(s[i]=='b') lb=i;
             else lc=i;
             if(la!=-1 and lb!=-1 and lc!=-1) count+=min({la,lb,lc})+1;
-            //redundant if condition, coz agar if nahi bhi lagaya and koi -1 hua toh min usko select karke +1 add kardega so resultant 0 hee add ho raha hoga
         }
         return count;
+
+        //M2 only but starting from n-1
+        // int count=0;
+        // int la=-1, lb=-1, lc=-1;
+        // for(int i=n-1; i>=0; i--){
+        //     if(s[i]=='a') la=i;
+        //     else if(s[i]=='b') lb=i;
+        //     else lc=i;
+        //     if(la!=-1 and lb!=-1 and lc!=-1) count+=min({la,lb,lc})+1;
+        // }
+        // return count;
     }
 };
