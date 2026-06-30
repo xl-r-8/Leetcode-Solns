1daccb32a0ef3df31cde11b7fc4096570b410236
xlr8 <abhi9arya8@gmail.com>
2026-06-30 23:31:17 +0530

    Time: 12 ms (22.85%) | Memory: 139.2 MB (12.74%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        //M1: O(n) TC and O(n) SC
-        int n=nums.size();
-        vector<int> neg;
+
         vector<int> pos;
-        for(int ele: nums){
-            if(ele<0)neg.push_back(ele);
-            else pos.push_back(ele);
+        vector<int> neg;
+
+        for(int x:nums){
+
+            if(x>0)
+                pos.push_back(x);
+            else
+                neg.push_back(x);
         }
-        int j=0, k=0;
-        vector<int> ans(n);
-        for(int i=0; i<n; i++){
-            if(i&1){
-                ans[i]=neg[j];
-                j++;    
-            }
-            else{
-                ans[i]=pos[k];
-                k++;
-            }
+
+        int p=0;
+        int n=0;
+        int i=0;
+
+        while(p<pos.size() && n<neg.size()){
+
+            nums[i++] = pos[p++];
+            nums[i++] = neg[n++];
         }
-        return ans;
+
+        while(p<pos.size())
+            nums[i++] = pos[p++];
+
+        while(n<neg.size())
+            nums[i++] = neg[n++];
+
+        return nums;
     }
 };
