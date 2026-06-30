46b88d4e0fdbdc5a56635317604957febdebaf1e
xlr8 <abhi9arya8@gmail.com>
2026-06-30 13:20:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (5.41%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -7,18 +7,19 @@ public:
         for(int i = n-2; i >= 0; i--){ //dont give spaces, it looks gptish. until and unless someone asks for it
             if(nums[i]<nums[i+1]){
                 int nge=nums[i+1]; int ngeind=i+1;
-                for(int j=i+2; j<n; j++){
-                    if(nums[j]>nums[i] and nums[j]<nge){
+                for(int j=n-1; j>=i; j--){
+                    if(nums[j]>nums[i] ){
                         nge=nums[j];
                         ngeind=j;
+                        break;
                     } 
                 }
                 swap(nums[i], nums[ngeind]);
-                sort(nums.begin()+i+1, nums.end());
+                reverse(nums.begin()+i+1, nums.end());
                 return;
             }
         }
-        sort(nums.begin(), nums.end());   
+        reverse(nums.begin(), nums.end());   
         return;
     }
 };
