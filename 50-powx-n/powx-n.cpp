4c1116124381cf7f4faebd651ec3b0f1ab2c70c0
xlr8 <abhi9arya8@gmail.com>
2026-07-07 15:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.03%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double p(double a, long long n){
+        if(n<0) n = - n;
+        double res = 1;
+        while(n){
+            if(n&1){
+                res = res*a;
+                n--;
+            }
+            else{
+                a = a*a;
+                n=n/2;
+            }
+        }
+        return res;
+    }
+    double myPow(double x, int n) {
+        //for +ve powers we can definitely use binary exponentiation
+        //but for -ve? x^-n = (1/x)^n
+        if(n>=0) return p(x, n);
+        
+        return p(1/x, n);
+    }
+};
