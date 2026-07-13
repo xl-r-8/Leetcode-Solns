06d51e95b2d3f353a921eca9226e34002e03221b
xlr8 <abhi9arya8@gmail.com>
2026-07-13 13:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (8.31%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void rec(int num, int low, int high, vector<int>& ans){
+        int ld = num%10;
+        if( num >= low and num <= high ) ans.push_back(num);
+        if( num == 0 ){
+            //start with every digit
+            for( int i = 1; i<=9; i++){
+                rec( i, low, high, ans );
+            }
+            return;
+        }
+        if( ld == 9 or num > high ) return;
+        num = 10*num + (ld+1);
+        rec(num, low, high, ans);
+    }
+
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+        // 10^9 => O(logn) or O(1)
+        // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 -> wont be used coz low > 10
+        // 12, 23, 34, 45, 67, 78, 89
+        // 123, 234, 345, 456, 567, 678, 789
+        // 1234, 2345, 3456, 4567, 5678, 6789
+        // 12345 23456 34567 45678 56789
+        // 123456 234567 345678 456789
+        // 1234567 2345678 3456789
+        // 12345678 23456789 
+        // 123456789
+        // all valid cases = 8+7+6+..+1 = 36, can definitely use recursion
+        rec(0, low, high, ans);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
