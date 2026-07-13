1fbb1c305e09cbdcd44d015155beda1f0fe9a162
xlr8 <abhi9arya8@gmail.com>
2026-07-13 13:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (8.31%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -31,5 +31,17 @@ public:
         rec(0, low, high, ans);
         sort(ans.begin(), ans.end());
         return ans;
+
+        //M2:
+        //instead of generating, make the list yourself and then just iterate
+        /* easiest way O(1) is to iterate all combinations:
+        12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789, 12345678, 23456789, 123456789 */
+
+        //M3: sliding window, Coolest
+        // Declare a string = "123456789" and think of sliding window.
+        string s = "123456789";
+        int i = 0, j = 1;
+
+
     }
 };
