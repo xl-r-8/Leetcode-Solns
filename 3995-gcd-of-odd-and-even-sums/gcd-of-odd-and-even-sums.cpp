066b05f15e9dc1db1b6a147d1aba1f6e44711bbe
xlr8 <abhi9arya8@gmail.com>
2026-07-15 14:27:37 +0530

    Time: 2 ms (18.99%) | Memory: 8.4 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
+        // The first n odd numbers sum to n * n
+        // Hint 2
+        // First n even numbers sum to n * (n + 1)
+        // Hint 3
+        // gcd(n, n + 1) = 1, so the answer is n
         return n;
     }
 };
