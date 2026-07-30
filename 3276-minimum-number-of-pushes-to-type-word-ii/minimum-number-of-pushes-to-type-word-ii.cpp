492a5c5d2e76a436257301a41646a4fc69324798
xlr8 <xlr8just@gmail.com>
2026-07-30 14:12:06 +0530

    Time: 6 ms (75.47%) | Memory: 25.4 MB (84.36%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,53 +1,23 @@
-#include <algorithm>
 class Solution {
 public:
     int minimumPushes(string word) {
-       /* map<char, int, greater<int>>  mp;
-        int i=0; //to count unique characters
-        // int count=0;
-        for(char c: word){
-            ++mp[c];
-        }       
-        // for(pair<char,int> p:mp){
-        //     count+=p.second*(i/8+1);
-        //     i++;
-        // }
-        // return count;
-        int totalPushes = 0;
-        int presses = 1;
-        int keysUsed = 0;
-
-        // Iterate through the frequencies in the map
-        for (const auto& p : mp) {
-            totalPushes += p.second * presses; // Count pushes for this character
-            keysUsed++;
-
-            // If 8 keys have been used, move to the next press count
-            if (keysUsed%8 == 0) {
-                // keysUsed = 0;  // Reset key usage
-                presses++;      // Increment presses needed for the next characters
-            }
-        }
-
-        return totalPushes;*/
-        int ans=0;
-        vector<int> freq(26,0); // frequenct calculated
-        for(auto it: word){
-            freq[it-'a']++;
-        }
-        sort(freq.begin(),freq.end(),greater<int>()); // sort frequency in decreasing order
-        for(int i=0;i<26;i++){
-            if(i<8){
-                ans+= freq[i]; // map at first positon of all keys
-            }
-            else if(i<16){
-                ans+= freq[i]*2; // map with second position of all keys
-            }else if(i<24){
-                ans+= freq[i]*3; // map with third position all keys
-            }
-            else ans+= freq[i]*4; // map with fourth position of all keys
+        //what brute force could be? maybe through recursion, idk.
+        //but my approach is to map the first 8 most frequent chars as 1st place in keys, then the next 8 as 2nd and then next 8 as 3rd and remaining 2 as 4th
+        //TC = O(nlogn), SC = O(n), idk if its the most optimal, or if its the brute force
         
+        vector<int> freq(26, 0);
+        for(char ch: word){
+            freq[ch-'a']++;
         }
+        sort(freq.begin(), freq.end(), greater<int>());
+        int ans = 0; 
+        // for(int ele: freq) cout<<ele<<" ";
+        // cout <<endl;
+        for(int i = 0; i<26; i++){
+            ans += freq[i] * (1 + i/8);
+            // num of chars * num of keypresses for 1 char
+        }
+
         return ans;
     }
 };
