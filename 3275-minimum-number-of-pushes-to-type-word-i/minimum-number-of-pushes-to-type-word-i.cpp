143e1a7868b8886244fbd670518f5e226e256dc3
xlr8 <xlr8just@gmail.com>
2026-07-30 11:43:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.67%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        //what brute force could be? maybe through recursion, idk.
-        //but my approach is to map the first 8 most frequent chars as 1st place in keys, then the next 8 as 2nd and then next 8 as 3rd and remaining 2 as 4th
-        //TC = O(nlogn), SC = O(n), idk if its the most optimal, or if its the brute force
-        
-        vector<int> freq(26, 0);
-        for(char ch: word){
-            freq[ch-'a']++;
+        //each char in the word is unique
+        int n = word.size(); int ans = 0;
+        if(n == 0) return 0;
+        while(n>0){
+            int x = n%8;
+            if(x == 0){
+                ans += 8 * ((n/8));
+                n = 8 * ((n/8)-1);
+            } 
+            else{
+                ans += x * ((n/8)+1);
+                n = 8 * ((n/8)); 
+            } 
+            // cout<<n<<" " <<x<<" "<<ans<<" "<<endl;
         }
-        sort(freq.begin(), freq.end(), greater<int>());
-        int ans = 0; 
-        // for(int ele: freq) cout<<ele<<" ";
-        // cout <<endl;
-        for(int i = 0; i<26; i++){
-            ans += freq[i] * (1 + i/8);
-            // num of chars * num of keypresses for 1 char
-        }
-
         return ans;
     }
 };
